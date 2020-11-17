06f524fb5ccf2c1a797ae64fc79e7ddbf20bdf01
FabioBer <58628606+FabioBer@users.noreply.github.com>
2020-11-17 11:40:18 +0100

    Update caltech_dataset.py

--- caltech_dataset.py
+++ caltech_dataset.py
@@ -29,6 +29,27 @@ class Caltech(VisionDataset):
           through the index
         - Labels should start from 0, so for Caltech you will have lables 0...100 (excluding the background class) 
         '''
+        assert(self.split in ['train','test'])
+        with open(f'./../{self.split}.txt') as f:
+            idx = f.readlines()
+            
+        # create (0..n, path) tuple list
+        
+        idx = [x.strip() for x in idx if 'background' not in x.lower()]
+        tup_idx_path = zip(range(len(idx)),idx)
+        
+        # create (0..n, class) tuple list
+        
+        classes = sorted(list(set([x.split('/')[0].strip() for x in idx])))
+        dict_classes = dict(zip(range(len(classes)),classes))
+        
+        tup_idx_class = dict()
+        for i in range(len(idx)):
+            tup_idx_class[i] = dict_classes[idx[i].split('/')[0].strip()]
+            
+        # merge into (path,class) tuple list
+        self.tup_class_path = [(tup_idx_classi][1],tup_idx_path[i][1]) for i in range(len(idx))]
+        
 
     def __getitem__(self, index):
         '''
@@ -39,8 +60,11 @@ class Caltech(VisionDataset):
         Returns:
             tuple: (sample, target) where target is class_index of the target class.
         '''
-
-        image, label = ... # Provide a way to access image and label via index
+        path = self.tup_class_path[index][1]
+        img = Image.open(open(os.path.join(self.root,path), 'rb'))
+        lab = self.tup_class_path[index][0]
+        image, label = img, lab
+                           # Provide a way to access image and label via index
                            # Image should be a PIL Image
                            # label can be int
 
@@ -55,5 +79,5 @@ class Caltech(VisionDataset):
         The __len__ method returns the length of the dataset
         It is mandatory, as this is used by several other components
         '''
-        length = ... # Provide a way to get the length (number of elements) of the dataset
+        length = len(self(tup_class_path)) # Provide a way to get the length (number of elements) of the dataset
         return length
