5de18fc6f77369fb6e7b82c176db1e2c38221e48
FabioBer <58628606+FabioBer@users.noreply.github.com>
2020-11-19 13:13:27 +0100

    Update caltech_dataset.py

--- caltech_dataset.py
+++ caltech_dataset.py
@@ -1,4 +1,3 @@
-
 from torchvision.datasets import VisionDataset
 
 from PIL import Image
@@ -6,6 +5,7 @@ from PIL import Image
 import os
 import os.path
 import sys
+import math
 
 
 def pil_loader(path):
@@ -16,57 +16,46 @@ def pil_loader(path):
 
 
 class Caltech(VisionDataset):
-    def __init__(self, root, split='train', transform=None, target_transform=None):
+    def __init__(self, root, transform=None, target_transform=None):
         super(Caltech, self).__init__(root, transform=transform, target_transform=target_transform)
 
-        self.split = split # This defines the split you are going to use
-                           # (split files are called 'train.txt' and 'test.txt')
-
-        '''
-        - Here you should implement the logic for reading the splits files and accessing elements
-        - If the RAM size allows it, it is faster to store all data in memory
-        - PyTorch Dataset classes use indexes to read elements
-        - You should provide a way for the __getitem__ method to access the image-label pair
-          through the index
-        - Labels should start from 0, so for Caltech you will have lables 0...100 (excluding the background class) 
-        '''
-        assert(self.split in ['train','test'])
-        with open(f'./../{self.split}.txt') as f:
-            idx = f.readlines()
+        self.root = root
+        self.transform = transform
+        self.target_transform = target_transform
+        
+        imgs = []
+        with open(os.path.join(root,'train.txt')) as f:
+            imgs = f.readlines()
+        with open(os.path.join(root,'test.txt')) as f:
+            for line in f:
+                imgs.append(line)
             
-        # create (0..n, path) tuple list
+        self.images = list()
+        for img in imgs:
+            if not str(img).startswith("BACKGROUND_Google/"):
+                self.images.append(img)
         
-        idx = [x.strip() for x in idx if 'background' not in x.lower()]
-        tup_idx_path = zip(range(len(idx)),idx)
+        self.length = len(self.images)
         
-        # create (0..n, class) tuple list
+        self.classes = os.listdir(self.root+'101_ObjectCategories')
+        self.classes.pop(self.classes.index('BACKGROUND_Google'))
         
-        classes = sorted(list(set([x.split('/')[0].strip() for x in idx])))
-        dict_classes = dict(zip(range(len(classes)),classes))
-        
-        tup_idx_class = dict()
-        for i in range(len(idx)):
-            tup_idx_class[i] = dict_classes[idx[i].split('/')[0].strip()]
+        self.tuple_classes = list() 
+        for i in range(len(self.classes)):
+            self.tuple_classes.append((i,self.classes[i]))
             
-        # merge into (path,class) tuple list
-        self.tup_idx_class = [(tup_idx_class[i][1],tup_idx_path[i][1]) for i in range(len(idx))]
-        
+        self.couples = list()
+        for img in self.images:
+            s = img.split('/')[0]
+            i = self.classes.index(s)
+            self.couples.append((i,img))
+            
 
     def __getitem__(self, index):
-        '''
-        __getitem__ should access an element through its index
-        Args:
-            index (int): Index
-        Returns:
-            tuple: (sample, target) where target is class_index of the target class.
-        '''
-        path = self.tup_idx_class[index][1]
-        img = Image.open(open(os.path.join(self.root,path), 'rb'))
-        lab = self.tup_idx_class[index][0]
-        image, label = img, lab
-                           # Provide a way to access image and label via index
-                           # Image should be a PIL Image
-                           # label can be int
+            
+        label,image = self.couples[index]
+        
+        image = pil_loader(self.root+'101_ObjectCategories/'+image.strip())
 
         # Applies preprocessing when accessing the image
         if self.transform is not None:
@@ -75,9 +64,6 @@ class Caltech(VisionDataset):
         return image, label
 
     def __len__(self):
-        '''
-        The __len__ method returns the length of the dataset
-        It is mandatory, as this is used by several other components
-        '''
-        length = len(self.tup_idx_class) # Provide a way to get the length (number of elements) of the dataset
+        
+        length = self.length
         return length
